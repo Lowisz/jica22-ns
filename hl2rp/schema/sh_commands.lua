9121c59c91adcfe8e8b01ef396a9ed4b9a4b2cf9
Lowisz <themegamanplays@gmail.com>
2017-11-10 22:14:15 -0300

    ow doorkick
    /owdoorkick fixed.

--- hl2rp/schema/sh_commands.lua
+++ hl2rp/schema/sh_commands.lua
@@ -39,14 +39,12 @@ nut.command.add("owdoorkick", {
 			local entity = util.TraceLine(data).Entity
 
 			if (IsValid(entity) and entity:GetClass() == "prop_door_rotating") then
-				if (client:forceSequence("kickdoorbaton")) then
-					timer.Simple(0.75, function()
-						if (IsValid(client) and IsValid(entity)) then
-							entity:EmitSound("physics/wood/wood_crate_break"..math.random(1, 5)..".wav", 150)
-							entity:blastDoor(aimVector * (360 + client:getChar():getAttrib("str", 0)*5))
-						end
-					end)
-				end
+				timer.Simple(0.75, function()
+					if (IsValid(client) and IsValid(entity)) then
+						entity:EmitSound("physics/wood/wood_crate_break"..math.random(1, 5)..".wav", 150)
+						entity:blastDoor(aimVector * (360 + client:getChar():getAttrib("str", 0)*5))
+					end
+				end)
 			else
 				return "@dNotValid"
 			end
